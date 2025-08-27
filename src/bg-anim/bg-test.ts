04cfb40c048b2b25a95614b6c1fb245de718bc85
Aaron <2858172+lith-x@users.noreply.github.com>
2025-08-26 20:54:13 -0500

    at least looks a tad better while it's live

--- src/bg-anim/bg-test.ts
+++ src/bg-anim/bg-test.ts
@@ -5,21 +5,18 @@ const CUBE_PADDING = 0.1;
 
 const vShader = `#version 300 es
 in vec3 position;
-// in vec3 instanceCenter;
-// in vec3 instanceColor;
+in vec3 instanceCenter;
+in vec3 instanceColor;
 in float instanceSize;
 
 uniform mat4 u_view;
 uniform mat4 u_proj;
-uniform float side_len;
-uniform vec3 offset;
-uniform vec4 color;
 
 out vec4 v_color;
 
 void main() {
-    gl_Position = u_proj * u_view * vec4(instanceSize * position + offset, 1.0);
-    v_color = color;
+    gl_Position = u_proj * u_view * vec4(instanceSize * position + instanceCenter, 1.0);
+    v_color = vec4(instanceColor, 1.0);
 }`;
 
 const fShader = `#version 300 es
@@ -29,18 +26,8 @@ in vec4 v_color;
 
 out vec4 outColor;
 
-// https://stackoverflow.com/a/17897228
-vec3 hsv2rgb(vec3 c)
-{
-    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
-
 void main() {
     outColor = v_color;
-    // outColor = vec4(1, 0, 0, 1);
-    // outColor = vec4(hsv2rgb(vec3(hue, 1.0, 1.0)), 1.0);
 }`;
 
 export const main = () => {
@@ -49,7 +36,16 @@ export const main = () => {
 
     const progInfo = twgl.createProgramInfo(gl, [vShader, fShader]);
 
-    const INSTANCE_COUNT = 50;
+    const INSTANCE_COUNT = 5;
+    const instanceCenters = new Float32Array(INSTANCE_COUNT * 3);
+    for (let i = 0; i < INSTANCE_COUNT; i++) {
+        instanceCenters.set([-0.5 + i / INSTANCE_COUNT, 0.4, 0], i * 3);
+    }
+    const instanceColors = new Float32Array(INSTANCE_COUNT * 4);
+    for (let i = 0; i < INSTANCE_COUNT; i++) {
+        // 134, 25, 181
+        instanceColors.set([134/255, 25/255, 181/255, 1], i * 4);
+    }
     const instanceSizes = new Float32Array(INSTANCE_COUNT);
     instanceSizes.fill(1);
     const rectArrays = {
@@ -72,32 +68,42 @@ export const main = () => {
                 0, 4, 1, 5, 2, 6, 3, 7
             ]
         },
+        instanceCenter: {
+            divisor: 1,
+            numComponents: 3,
+            data: instanceCenters
+        },
+        instanceColor: {
+            divisor: 1,
+            numComponents: 4,
+            data: instanceColors
+        },
         instanceSize: {
             divisor: 1,
             numComponents: 1,
             data: instanceSizes
-        }
+        },
+
     } as twgl.Arrays;
-    const cubeUniforms = {
-        scale: 1.0
-    };
     const rectBuff = twgl.createBufferInfoFromArrays(gl, rectArrays);
     const vao = twgl.createVAOFromBufferInfo(gl, progInfo, rectBuff);
     const viewMat = twgl.m4.inverse(twgl.m4.lookAt([0, 0, 2], [0, 0, 0], [0, 1, 0]));
     const projMat = twgl.m4.perspective(Math.PI / 2.5, /*gl.drawingBufferWidth / gl.drawingBufferHeight*/1, 0.1, 100);
     gl.useProgram(progInfo.program);
     gl.bindVertexArray(vao);
-    twgl.setUniforms(progInfo, { u_view: viewMat, u_proj: projMat, side_len: 0.5, color: [1, 1, 0, 1], offset: [0.6, 0.4, 0] });
-    let size = 0;
+    twgl.setUniforms(progInfo, { u_view: viewMat, u_proj: projMat });
+
     const render = (time: number) => {
         twgl.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
         gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-        gl.clearColor(0, 0, 0, 1);
+        // gl.clearColor(0, 0, 0, 1);
         gl.clear(gl.COLOR_BUFFER_BIT);
         for (let i = 0; i < instanceSizes.length; i++) {
-            instanceSizes[i] = 3 + Math.sin(time / 1000);
+            instanceSizes[i] = 3 + Math.sin(time / 1000) + i;
         }
+        twgl.setAttribInfoBufferFromArray(gl, rectBuff.attribs!.instanceCenter, instanceCenters);
         twgl.setAttribInfoBufferFromArray(gl, rectBuff.attribs!.instanceSize, instanceSizes);
+        twgl.setAttribInfoBufferFromArray(gl, rectBuff.attribs!.instanceColor, instanceColors);
         twgl.drawBufferInfo(gl, rectBuff, gl.LINES, undefined, undefined, INSTANCE_COUNT);
         requestAnimationFrame(render);
     };
